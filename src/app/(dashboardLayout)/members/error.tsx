7ae1c2142adaed8ae3c4cf3e72606556af0a7119
Mehebul02 <mehebul2123@gmail.com>
2025-03-08 00:08:51 +0600

    Added the error page

---
+++ src/app/(dashboardLayout)/members/error.tsx
@@ -0,0 +1,22 @@
+
+interface ErrorProps{
+    error:string;
+    reset:()=>void
+}
+const error = ({error,reset}:ErrorProps) => {
+    return (
+        <div>
+            <h1>{error}</h1>
+            <button
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </button>
+        </div>
+    );
+};
+
+export default error;
---
+++ src/app/(dashboardLayout)/members/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MembersPages = () => {
+    return (
+        <div>
+             <h1>Member page</h1>   
+        </div>
+    );
+};
+
+export default MembersPages;
